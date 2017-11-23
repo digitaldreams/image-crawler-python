a59b46b3dd11268600a25a5c76d6a6650b0349cb
Tuhin Bepari <digitaldreams40@gmail.com>
2017-11-23 20:53:04 +0600

    Validate Url after fetch and Page Remove

--- Crawler/Page.py
+++ Crawler/Page.py
@@ -3,28 +3,18 @@ import urllib.parse
 import os.path
 import functions
 from Crawler.LinkFinder import LinkFinder
-import Models.Queue.Page
-import Models.Complete.Page
 import Models.Queue.Link
 
 
 class Page:
     def __init__(self, page_url):
-        queue = Models.Queue.Page.Page
-        dir(queue)
-        queue.fetch()
-        self.queue = queue
-
-        complete = Models.Complete.Page.Page
-        complete.fetch()
-        self.complete = complete
 
         link = Models.Queue.Link.Link
         link.fetch()
         self.link = link
 
         self.page_url = page_url
-        self.queue.add(page_url)
+        self.link.add(page_url)
         urlres = urllib.parse.urlparse(page_url)
         self.base_url = urlres.netloc
         self.scheme = urlres.scheme
@@ -33,7 +23,7 @@ class Page:
     def add(self, link):
         full_url = self.sanitize_url(link)
         if full_url:
-            self.queue.add(full_url)
+            self.link.add(full_url)
         return self
 
     def fetch_links(self):
@@ -51,22 +41,17 @@ class Page:
 
     def _merge_links(self):
         for lk in self.links:
-            self.link.add(lk)
+            if self.validate_url(lk):
+                self.link.add(lk)
         return self.link.links
 
-    def queued(self):
-        return self.queue.links
-
-    def completed(self):
-        self.complete.links
-
     def save(self):
-        self.queue.save()
+        self._merge_links()
+        self.link.save()
 
     def save_links(self):
         self.fetch_links()
-        self._merge_links()
-        self.link.save()
+        self.save()
 
     def get_base_url(self) -> object:
         """
@@ -82,3 +67,7 @@ class Page:
         :return:
         """
         return urllib.parse.urljoin(self.scheme + "://" + self.base_url, url)
+
+    def validate_url(self, url):
+        netloc = urllib.parse.urlparse(url).netloc
+        return self.base_url == netloc
